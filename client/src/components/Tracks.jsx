d5f3e2ac84ce52a6ab86cfda8b54109b24c1416a
chrisreylo73 <chrisreylo73@gmail.com>
2023-07-21 00:47:06 -0600

    Fixed hover song events and css

--- client/src/components/Tracks.jsx
+++ client/src/components/Tracks.jsx
@@ -1,6 +1,7 @@
 import SpotifySong from "./SpotifySong";
 import YoutubeSong from "./YoutubeSong";
 import { useState } from "react";
+
 const Tracks = ({ spotifySelectedPlaylist, youtubeSelectedPlaylist, youtubePlaylistTracks, youtubeChooseTrack, spotifyPlaylistTracks, spotifyChooseTrack }) => {
 	const [isHovered, setIsHovered] = useState(false);
 	const handleMouseEnter = () => {
@@ -19,13 +20,9 @@ const Tracks = ({ spotifySelectedPlaylist, youtubeSelectedPlaylist, youtubePlayl
 				<div className="playlistHeaders-youtube">
 					<h3 style={{ textTransform: "uppercase" }}>{youtubeSelectedPlaylist.playlistTitle}</h3>
 				</div>
-				<div className="tracks-content">
-					<ul>
-						{youtubePlaylistTracks.map((youtubePlaylistTrack) => {
-							return <YoutubeSong youtubeChooseTrack={youtubeChooseTrack} track={youtubePlaylistTrack} />;
-						})}
-					</ul>
-				</div>
+				{youtubePlaylistTracks.map((youtubePlaylistTrack) => (
+					<YoutubeSong youtubeChooseTrack={youtubeChooseTrack} track={youtubePlaylistTrack} />
+				))}
 			</div>
 		);
 	} else if (youtubeSelectedPlaylist == null) {
@@ -34,14 +31,10 @@ const Tracks = ({ spotifySelectedPlaylist, youtubeSelectedPlaylist, youtubePlayl
 				<div className="playlistHeaders-spotify">
 					<h3 style={{ textTransform: "uppercase" }}>{spotifySelectedPlaylist.name}</h3>
 				</div>
-				<div className="tracks-content">
-					<ul>
-						{spotifyPlaylistTracks.map((playlistTrack) => {
-							const track = playlistTrack.track;
-							return <SpotifySong key={track.id} spotifyChooseTrack={spotifyChooseTrack} track={track} />;
-						})}
-					</ul>
-				</div>
+				{spotifyPlaylistTracks.map((playlistTrack) => {
+					const track = playlistTrack.track;
+					return <SpotifySong key={track.id} spotifyChooseTrack={spotifyChooseTrack} track={track} />;
+				})}
 			</div>
 		);
 	} else {
@@ -50,25 +43,17 @@ const Tracks = ({ spotifySelectedPlaylist, youtubeSelectedPlaylist, youtubePlayl
 				<div className="playlistHeaders-spotify">
 					<h3 style={{ textTransform: "uppercase" }}>{spotifySelectedPlaylist.name}</h3>
 				</div>
-				<div className="tracks-content">
-					<ul>
-						{spotifyPlaylistTracks.map((playlistTrack) => {
-							const track = playlistTrack.track;
-							return <SpotifySong spotifyChooseTrack={spotifyChooseTrack} track={track} />;
-						})}
-					</ul>
-				</div>
+				{spotifyPlaylistTracks.map((playlistTrack) => {
+					const track = playlistTrack.track;
+					return <SpotifySong spotifyChooseTrack={spotifyChooseTrack} track={track} />;
+				})}
 
 				<div className="playlistHeaders-youtube">
 					<h3 style={{ textTransform: "uppercase" }}>{youtubeSelectedPlaylist.playlistTitle}</h3>
 				</div>
-				<div className="tracks-content">
-					<ul>
-						{youtubePlaylistTracks.map((youtubePlaylistTrack) => {
-							return <YoutubeSong youtubeChooseTrack={youtubeChooseTrack} track={youtubePlaylistTrack} />;
-						})}
-					</ul>
-				</div>
+				{youtubePlaylistTracks.map((youtubePlaylistTrack) => (
+					<YoutubeSong youtubeChooseTrack={youtubeChooseTrack} track={youtubePlaylistTrack} />
+				))}
 			</div>
 		);
 	}
